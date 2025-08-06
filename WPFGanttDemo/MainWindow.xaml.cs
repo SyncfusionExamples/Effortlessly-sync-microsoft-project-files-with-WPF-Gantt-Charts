96142d4ccdbc8422f57692cde3d87228bf06f8eb
Karthick M <karthick.mani@syncfusion.com>
2025-08-06 21:58:08 +0530

    Gantt Chart import and export sample added.

---
+++ WPFGanttDemo/MainWindow.xaml.cs
@@ -0,0 +1,35 @@
+﻿namespace WPFGanttDemo
+{
+    using System.Windows;
+    using Syncfusion.SfSkinManager;
+
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            SfSkinManager.SetTheme(this, new Theme() { ThemeName = "Material3Light" });
+        }
+
+        private void OnExportButtonClick(object sender, RoutedEventArgs e)
+        {
+            if (this.ganttControl.ExportToXML())
+            {
+                MessageBox.Show("Tasks exported successfully.", "XML Import/Export",
+                    MessageBoxButton.OK, MessageBoxImage.Information);
+            }
+        }
+
+        private void OnImportButtonClick(object sender, RoutedEventArgs e)
+        {
+            if (this.ganttControl.ImportFromXML())
+            {
+                MessageBox.Show("Tasks imported successfully.", "XML Import/Export", 
+                    MessageBoxButton.OK, MessageBoxImage.Information);
+            }
+        }
+    }
+}
